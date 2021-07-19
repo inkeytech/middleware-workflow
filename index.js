a8d1e22e46707c68f768447fdd9bc1614838ceac
Ian Watson <iwatson@inkey.tech>
2021-07-19 23:36:20 +0200

    refactored stub

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+//Includes------------------------------------------------------------------------------------------------------
+// var extend = require('extend');
+var _ = require('lodash');
+// var moment = require('moment-timezone');
+var async = require('async');
+// const axios = require('axios');
+// const url = require('url');
+var pjson = require('./package.json');
+// --------------------------------------------------------------------------------------------------
+
+var glOptions;
+const defDefshema = {
+	user: {},
+	parameter: {},
+	object: {},
+	objectSet: {},
+	message: {},
+	messageSets: {},
+	record: {},
+	recordSet: {},
+	lookup: {},
+	binary: {},
+	spreadsheet: {}
+};
+
+
+
 var fnMutate3 = function(meta, next){
 	try {
 		meta.input.parameters.ruler = 'Ian the Fucking Great';
@@ -7,23 +34,116 @@ var fnMutate3 = function(meta, next){
 	}
 };
 
-var wrapFnMutate3 = wrapFunction(fnMutate3);
+var triggerFunction = function(triggerData, meta, next){
+	try {
+		meta.input.parameters.ruler = 'Ian the Fucking Great';
+		next();
+	} catch(err){
+		next(err);
+	}
+}
 
-function wrapFunction(fnIn){
-	return function(meta){
+
+
+module.exports = function(schema = defDefshema, fnTriggerMapper, moduleOptions){
+	var defOptions = {};
+	glOptions = _.extend(defOptions, moduleOptions);
+	glOptions.schema = schema;
+	glOptions.fnTriggerMapper = fnTriggerMapper;
+	
+	
+	var module = {
+		run: fnMiddleware,
+		version: fnGetVersion
+	};
+	
+	return module;
+};
+
+
+// ------------------------     BASE FUNCTIONS ------------------------------------
+
+
+function fnMiddleware(triggerObject, arMiddleware){
+	var ctx = {
+		meta: glOptions.schema,
+		outputs: []
+	};
+	
+	var fnPrmTrigger = wrapTriggerFunction(triggerObject, glOptions.fnTriggerMapper);
+	var arPrmFunctions = [];
+	arMiddleware.forEach(function(fnMiddleware){
+		arPrmFunctions.push(wrapFunction(fnMiddleware));
+	});
+	
+	
+	fnPrmTrigger(triggerObject, ctx.meta)
+	.then(function(meta){
+		ctx.meta = meta;
 		return new Promise(function(fulfill, reject){
-			function fnNext(err){
+			async.reduce(arPrmFunctions, ctx.meta, function(meta, item, callback){
+				item(meta)
+				.then(function(meta){
+					callback(null, meta);
+				})
+				.catch(function(err){
+					callback(err);
+				});
+			}, function(err, result){
 				if (err){
 					reject(err);
 				}
-				fulfill(meta);
-			}
-			
-			try {
-				fnIn(meta, fnNext);
-			} catch(err){
-				reject(err);
-			}
-		}); //end of promise
-	}; //end of returned function
+				fulfill(result);
+			});
+		});
+	});
+
+	
+	
+	
+	function wrapFunction(fnIn){
+		return function(meta){
+			return new Promise(function(fulfill, reject){
+				function fnNext(err){
+					if (err){
+						reject(err);
+					}
+					fulfill(meta);
+				}
+				
+				try {
+					fnIn(meta, fnNext);
+				} catch(err){
+					reject(err);
+				}
+			}); 
+		}; 
+	}
+	
+	function wrapTriggerFunction(triggerObject, mapperFunction){
+		return function(meta){
+			return new Promise(function(fulfill, reject){
+				function fnNext(err){
+					if (err){
+						reject(err);
+					}
+					fulfill(meta);
+				}
+				try {
+					mapperFunction(triggerObject, meta, fnNext);
+				} catch(err){
+					reject(err);
+				}
+			}); 
+		}; 
+	}
 }
+
+
+function fnGetVersion(){
+	return pjson.version;
+}
+
+// function isObject(a){
+// 	return (!!a) && (a.constructor === Object);
+// }
